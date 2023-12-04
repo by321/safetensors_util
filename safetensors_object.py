097ab7bc981f00774a24377ccbf7db91996337f0
Duane Moody <duane_moody@yahoo.com>
2023-12-04 12:34:42 -0700

    Add documentation to safetensors_object.py
    Examples of how it's used and the data it returns.

--- safetensors_object.py
+++ safetensors_object.py
@@ -2,6 +2,28 @@ import json, sys
 from safetensors_file import SafeTensorsFile
 from safetensors_worker import _ParseMore
 
+"""
+$ python3 safetensors_m.py ss_network_module /path/to/file/summer_dress.safetensors
+"networks.lora"
+
+$ python3 safetensors_m.py nonexistent_module /path/to/file/summer_dress.safetensors
+Error: Metadata does not contain a `nonexistent_module` item, did you spell it right?
+
+$ python3 safetensors_m.py ss_network_module /path/to/file/weird_file.safetensors
+Error: File header does not contain a `__metadata__` item
+
+$ python3 safetensors_m.py ss_tag_frequency /path/to/file/trina.safetensors
+{
+    "6_trina": {
+        "trina": 26, 
+        " black hair": 20, 
+        " hands on hips": 1, 
+        " looking at viewer": 7
+    }
+}
+
+"""
+
 def get_object(tensorsfile: str) -> str:
 	s = SafeTensorsFile.open_file(tensorsfile, quiet=True)
 	js = s.get_header()
