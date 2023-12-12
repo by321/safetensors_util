27e49df9fdd8b07d19109027703e3a528c79a369
by321 <by321@hotmail.com>
2023-12-11 22:08:08 -0800

    bump version number

--- safetensors_util.py
+++ safetensors_util.py
@@ -17,7 +17,7 @@ quiet_flag=click.option("-q","--quiet",default=False,is_flag=True, show_default=
                         help="Quiet mode, don't print informational stuff" )
 
 @click.group()
-@click.version_option(version=3)
+@click.version_option(version=6)
 @quiet_flag
 
 @click.pass_context
